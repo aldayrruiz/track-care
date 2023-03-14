5dc0bcfd1acf2691747f3a03f1e255785c74ac44
Aldayr Ruiz <aldayr2000@gmail.com>
2023-03-14 18:54:58 +0100

    chore: Add swagger & husky

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerDocumentBuilder } from './swagger/builder';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	const swaggerBuilder = new SwaggerDocumentBuilder(app);
+	swaggerBuilder.build();
+	await app.listen(3000);
 }
 bootstrap();
---
+++ backend/src/swagger/builder.ts
@@ -0,0 +1,34 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export class SwaggerDocumentBuilder {
+	private app: INestApplication;
+
+	constructor(app: INestApplication) {
+		this.app = app;
+	}
+
+	public build() {
+		const config = this.getConfig();
+		const document = this.createDocument(config);
+		this.setup(document);
+	}
+
+	private getConfig() {
+		const config = new DocumentBuilder()
+			.setTitle('BLUE Watch')
+			.setDescription('BLUE Watch API description')
+			.setVersion('1.0')
+			.build();
+		return config;
+	}
+
+	private createDocument(config) {
+		const document = SwaggerModule.createDocument(this.app, config);
+		return document;
+	}
+
+	private setup(document) {
+		SwaggerModule.setup('api/docs', this.app, document);
+	}
+}
